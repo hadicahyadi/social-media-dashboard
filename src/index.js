f37369842210574c60bafda7bab5035d46b15e8f
Hadi Cahyadi <hadihadihadi91@gmail.com>
2019-06-21 17:18:35 +0700

    Add entry point

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+  render () {
+    return (
+      <div class="app">
+        
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+	<App/>,
+	document.getElementById('app')
+);
