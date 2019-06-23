2e9fbf1f93e4f44ae2ef4aa56c3e43fafadd8027
Hadi Cahyadi <hadihadihadi91@gmail.com>
2019-06-23 20:03:54 +0700

    Use HashRouter instead BrowserRouter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,6 +5,7 @@ import Container from '@material-ui/core/Container';
 import Home from '@/pages/Home';
 import Users from '@/pages/Users';
 import Posts from '@/pages/Posts';
+import PostDetail from '@/pages/PostDetail';
 import Albums from '@/pages/Albums';
 
 function AppRouter() {
@@ -12,9 +13,10 @@ function AppRouter() {
     <Container>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/users" component={Users} />
+        <Route path="/users" component={Users} />
         <Route exact path="/posts" component={Posts} />
-        <Route exact path="/albums" component={Albums} />
+        <Route path="/posts/:id" component={PostDetail} />
+        <Route path="/albums" component={Albums} />
       </Switch>
     </Container>
   )
